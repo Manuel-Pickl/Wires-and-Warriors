2346eed5070a3f739fbe697ab203891b4a2ec10a
Manuel Pickl <picklmanuel98@gmail.com>
2023-07-17 18:39:26 +0200

    higher volumne

--- lib/constants.cpp
+++ lib/constants.cpp
@@ -4,6 +4,6 @@ const int playerStartingHearts = 5;
 const int errorCooldown = 2000;
 const int flashsPerError = 5;
 const int earliestFinishTime = 1000; // milliseconds
-const int volumne = 3; // 0-30
+const int volumne = 20; // 0-30
 
 const int lifeLEDsCount = 24;
