356cb068d7513433d111edb3806849cd161a630d
Manuel Pickl <picklmanuel98@gmail.com>
2023-11-14 11:40:12 +0100

    enable audio

--- lib/gameConstants/gameConstants.cpp
+++ lib/gameConstants/gameConstants.cpp
@@ -8,7 +8,7 @@ const int flashsPerError = 5;
 const int earliestFinishTime = 1000; // milliseconds
 const int volumne = 20; // 0-30
 
-const int lifeLEDsCount = 24;
+const int lifeLEDsCount = 6;
 
 // debug
 const bool showLogs = true;
--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ void setup() {
   setOutputPinModes();
 
   initializeGame();
-  // initializeAudio();
+  initializeAudio();
 
   log("game initialized");
 }
